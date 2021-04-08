adc2390b349994bb5a9c5c4484fe9e6db467dd69
Yanis Hermassi <yanis.hermassi@gmail.com>
2021-04-08 02:16:40 +0100

    add a response body to register method

--- AaaaperoBack/Controllers/UserController.cs
+++ AaaaperoBack/Controllers/UserController.cs
@@ -139,7 +139,30 @@ namespace AaaaperoBack.Controllers
             {
                 // create user
                 _userService.Create(user, model.Password);
-                return Ok();
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(Configuration["Secret"]);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, user.Id.ToString()),
+                        new Claim(ClaimTypes.Role, user.AccessLevel ?? "null")
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(7),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = tokenHandler.WriteToken(token);
+
+                // return basic user info and authentication token
+                return Ok(new
+                {
+                    Id = user.Id,
+                    Username = user.Username,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Token = tokenString
+                });
             }
             catch (AppException ex)
             {
