fb58e6fd7839f06914945c81d17e0bd597843582
Lucas Theze <lucas64450@hotmail.fr>
2021-04-08 18:52:29 +0100

    change admin role

--- AaaaperoBack/DTO/Role.cs
+++ AaaaperoBack/DTO/Role.cs
@@ -3,7 +3,7 @@ namespace AaaaperoBack.DTO
     public class Role
     {
         public const string SuperUser = "Super User";
-        public const string Admin = "Admin, Super User";
+        public const string Admin = "Admin";
         public const string Candidate = "Candidate";
         public const string Employer = "Employer";
 
